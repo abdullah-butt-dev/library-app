2a9a768a9e4253b8a0c4f83bbada7a310c140107
abdullah <230536727+abdullah-butt-dev@users.noreply.github.com>
2025-11-15 15:19:15 +0500

    Add dynamic html using javascript

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+const bookContainer = document.querySelector('.book-container');
 const myLibrary = [];
 
-function Book(id, title, author, pages, readStatus) {
+function Book(imageLink, id, title, author, pages, readStatus) {
   
   if(!new.target) {
     throw Error('Use new operator to create an object');
   }
   
+  this.imageLink = imageLink;
   this.id = id;
   this.title = title;
   this.author = author;
@@ -15,27 +17,35 @@ function Book(id, title, author, pages, readStatus) {
 }
 
 
-function addBookToLibrary(title, author, pages, readStatus) {
+function addBookToLibrary(imageLink, title, author, pages, readStatus) {
 
   const id = crypto.randomUUID();
-  console.log(id);
-  const book = new Book(id, title, author, pages, readStatus);
-  console.log(book);
-
+  const book = new Book(imageLink, id, title, author, pages, readStatus);
   myLibrary.push(book);
+
 }
 
 function displayBooks() {
   for(const book of myLibrary) {
-    console.log(book.id);
-    console.log(book.title);
-    console.log(book.author);
-    console.log(book.pages);
-    console.log(book.readStatus);
+
+    bookContainer.innerHTML += `
+      <div class="book-card">
+          <div class="book-img">
+            <img src="${book.imageLink}" alt="Book">
+          </div>
+          <div class="book-title">${book.title}</div>
+          <div class="book-author">by ${book.author}</div>
+          <div class="book-pages">Pages: ${book.pages}</div>
+          <div class="book-readStatus">Read: ${book.readStatus}</div>
+          <button class="remove-btn">Remove</button>
+          <button class="mark-btn">Mark Read</button>
+      </div>
+    `;
+
   }
 }
 
-addBookToLibrary('The Murder of History', 'K.K.Aziz', 200, 'Yes')
-addBookToLibrary('1986', 'George Orwell', 327, 'No')
-addBookToLibrary('Ego is the enemy', 'Ryan Holiday', 180, 'No')
+addBookToLibrary('https://razarumi.com/wp-content/uploads/2022/03/the-murder-of-history-600x600-1.png', 'The Murder of History', 'K.K.Aziz', 200, 'Yes');
+addBookToLibrary('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRGTbpydDXhx0pgNMlb480AbauiFjPziZdLQ&s', '1984', 'George Orwell', 327, 'No');
+addBookToLibrary('https://readings-storage.s3.ap-south-1.amazonaws.com/images/9781591847816.webp', 'Ego is the enemy', 'Ryan Holiday', 180, 'No');
 displayBooks();
